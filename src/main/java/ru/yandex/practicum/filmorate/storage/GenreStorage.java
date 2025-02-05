eaab3cd483df904216c979a514c92d6218292b83
Umid <umidjoninagamov@yandex.ru>
2025-02-05 10:39:12 +0500

    Final commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -4,7 +4,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
-import java.util.Set;
 
 public interface GenreStorage extends CommonStorage<Genre> {
     List<Genre> getGenresByFilm(Film film);
